e0c159d34121f26870cdab867e479bc9bb5e5dd0
ellylabay <elabay@ma.org>
2018-02-14 08:57:16 -0800

    test

--- Dictionaries/Histogram.py
+++ Dictionaries/Histogram.py
@@ -1,6 +1,8 @@
+# Elly Labay taking a string counting and storing the number of occurances of the letters and then making a histogram of the letter values with dictionaries
+import turtle
 import string
-def clean(text):
-    #num =len(text)
+
+def clean(text): # Clean the data making it all lowercase and taking out all punctuation
     Alph = string.ascii_lowercase
     new_string= ""
     for char in text:
@@ -19,33 +21,58 @@ def countAll(data):
     return  c_dic
 
 def sort_list(dic):
-
     s_keys = list(dic.keys())
     s_keys.sort()
-
-
     return s_keys
 
 def sorted_dict(old_dic, list):
     new_dic={}
     for letter in list:
         if letter in old_dic:
-            print(letter)
+            #print(letter)
             value = old_dic[letter]
-            print(value)
-            new_dic[letter]+= value
-    print (old_dic)
+           # print(value)
+            new_dic[letter] = value
     return new_dic
-
+def barChart(t, value, letter):
+    t.pensize(3)
+   # t.speed (0)
+    t.fillcolor("blue")
+    t.begin_fill()
+    t.left(90)
+    t.forward(value)
+    t.right(90)
+    t.forward(20)
+    t.write(str(value), align="left", font = ("Arial", 20, "normal"))
+    t.forward(30)
+    t.right(90)
+    t.forward(value)
+    t.left(90)
+    t.end_fill()
+    t.penup()
+    t.backward(25)
+    t.write(str(letter), align="left", font = ("Arial", 20, "normal"))
+    t.forward(25)
+    t.pendown()
 
 def main():
+    wn = turtle.Screen()
+    fin = turtle.Turtle()
+    fin.shape("turtle")
     file = open("Test Text file_Guillotine", "r")
     text_file = file.read()
     diction = countAll(clean(text_file))
-    list= sort_list(countAll(clean(text_file)))
-    print(diction)
-    print(list)
-    print(sorted_dict(diction, list))
-    #print((clean(text_file)))
-   # print(list)
+    list = sort_list(countAll(clean(text_file)))
+
+    data = sorted_dict(diction, list)
+    data_num = data.values()
+    largest_data = max(data_num)
+    data_points = len(data_num)
+
+    border = 15
+    wn.setworldcoordinates(0-border, 0-border, 50*data_points+border, largest_data+border)
+
+    for i in data:
+       # print (i, data[i])
+        barChart(fin, data[i], i)
 main()
--- Unit1_strings/Files Ch. 11/MadLibs_.py
+++ Unit1_strings/Files Ch. 11/MadLibs_.py
@@ -1,11 +1,12 @@
 #Elly Labay 2/2/18
+import time
 
-def mad_lists(data):
+def mad_lists(data,partofspeech):
     #print(data)
-    partofspeech = ["(time)", "(adj)", "(sound)", "(verb-past)", "(verb-ING)", "(adverb)","(noun)", "(verb-ED)", "(number)"]
     p_num = len(partofspeech)
     completed = ""
     line = data.split()
+    num = len(line)
     i = 0
     for word in line:
         #print(word)
@@ -14,42 +15,58 @@ def mad_lists(data):
         #print(place)
             user = input(partofspeech[i])
             line[place] = user
-          #  print(line)
-           # if i == 5:
-               # print (place)
+            print(i)
         else:
             i +=1
-            if i >=9:
+            if i >=11:
                 final = " ".join(line)
                 return final
-def mad_dic(data):
-    partofspeech = ["(time)", "(adj)", "(sound)", "(verb-past)", "(verb-ING)", "(adverb)","(noun)", "(verb-ED)", "(number)" ]
+
+
+
+def mad_dic(data, partofspeech):
     line = data.split()
     dic ={}
-    replace = ""
+    #replace = ""
     for i in partofspeech:
         user = input(i)
         dic[i] = user
-    print(dic)
+    #print(dic)
     for word in line:
-        if dic[word] is True:
-            print (dic[word])
-            print [word]
-            place = line.index(dic[word])
-            line[place] = dic[word]
-            print(line[word])
-            print("true")
+        if word in dic:
+            #print (dic[word])
+           # print [word]
+            place = line.index(word)
+            line.insert(place, dic[word])
+            #line[place] = dic[word]
+           # print(line[word])
+           # print(word)
+            print(word)
         else:
-            replace += word
-            print (replace)
+            print(9435678)
+            time.sleep(5)
+            None
 
-   # final = " ".join(replace)
-    return replace
+
+    final = " ".join(line)
+    print(final)
+
+def d_mad(data, partofspeech):
+    dic ={}
+    for i in partofspeech:
+        user = input(i)
+        dic[i] = user
+
+
+    return
+
+
 
 
 
 
 def main():
+    parts = ["(adj)","(sounds)", "(sound)", "(verb-past)", "(verb-ING)", "(adverb)","(noun)", "(verb-ED)", "(number)", "-(adj)", "-(verb-ED)"]
     file = open("madlib_StrangeAnimal", "r")
     test_file = open("test d mad", "r")
 
@@ -57,9 +74,10 @@ def main():
     test = test_file.read()
     #print(mad_temp)
 
-    #print(mad_lists(mad_temp))
-    #print(mad_dic(mad_temp))
-    print(mad_dic(test))
+    #print(mad_lists(mad_temp,parts))
+    print(mad_dic(mad_temp, parts))
+   # print(mad_dic(test))
 
-
+   # print( d_mad(mad_temp, parts))
+    #print(type(mad_temp))
 main()
