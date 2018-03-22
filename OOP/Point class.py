95db75cc0a075dbe39019381b8ac595f85e5131f
ellylabay <elabay@ma.org>
2018-03-22 08:23:15 -0700

    test

--- OOP/Point class.py
+++ OOP/Point class.py
@@ -32,23 +32,32 @@ class Point:
         self.x = self.x + dx
         self.y = self.y + dy
 
+    def circle_3(self,x2,y2,x3,y3):
+        x1 = self.x
+        y1 = self.y
+        Tail = (2*(x1*(y2-y3)- y1*(x2-x3) +(x2*y3) - (x3*y2)))
+        circle_center =(x1**2 + y1**2)*(y2-y3)+(x2**2+y2**2)*(y3-y1)+(x3**2+y3**2)*(y1-y2)/ Tail
+
+        X = str(circle_center)
+
+        return X
 
     def __str__(self):
         return "(" + str(self.x)+ "," + str(self.y) + ")"
 
-p = Point(0,0)
+p = Point(0,-5)
 other = Point(1,1)
 
 print(p)
-print(other)
+#print(other)
 
 dis = p.distance_to_point(other)
-
 #print(dis)
 
 #print (other.reflect_x())
 
 #print(Point(4,11).get_line_to(Point(6,15)))
-p.move(7, 10)
-print(p)
+#p.move(7, 10)
+center_x= p.circle_3(0,5,5,0)
+print(center_x)
 
